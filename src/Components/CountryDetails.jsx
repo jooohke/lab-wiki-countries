7187331fd28aa759294822586e27356e61e033cb
Johanna Angot <63612183+jooohke@users.noreply.github.com>
2021-08-13 09:53:16 +0200

    iteration 1 done

--- src/Components/CountryDetails.jsx
+++ src/Components/CountryDetails.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
+import countries from "../countries.json"
 
 class CountryDetails extends Component {
     render() {
-        console.log(this.props.countries)
         const getCountry = (cca3) => {
             const theCountry = (oneCountry) => {
                 return oneCountry.cca3 === cca3
             };
-            return this.props.countries.find(theCountry)
+            return countries.find(theCountry)
         };
 
         const { params } = this.props.match;
@@ -17,7 +17,29 @@ class CountryDetails extends Component {
 
         return (
             <div>
-            <h1>CountryDetails -</h1>
+            <h1>{foundCountry.name.common}</h1>
+
+            <table>
+                <tr>
+                    <td>Capital</td>
+                    <td>{foundCountry.capital}</td>
+                </tr>
+                <tr>
+                    <td>Area</td>
+                    <td>{foundCountry.area} km²</td>
+                </tr>
+                <tr>
+                    <td>Borders</td>
+                    <td>
+                        <ul>
+                        {foundCountry.borders.map((country) => {
+                        return (<li key={country} style={{listStyle:"none"}}>{country}</li>)
+                    })}
+                        </ul>
+                        </td>
+                </tr>
+
+            </table>
             
         </div>
         )
